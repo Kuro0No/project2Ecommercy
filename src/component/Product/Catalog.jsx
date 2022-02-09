45bfe583fa1a1e6e1c6fb677e8973cf46b47ce7f
Kuro0No <90337201+Kuro0No@users.noreply.github.com>
2022-02-09 19:47:20 +0700

    1

--- src/component/Product/Catalog.jsx
+++ src/component/Product/Catalog.jsx
@@ -1,5 +1,4 @@
-import React, { useContext } from 'react';
-import { useState } from 'react/cjs/react.development';
+import React, { useContext,useState } from 'react';
 import { memo } from 'react';
 import { productContext } from '../../ProductContext/ProductContext';
 import { useLocation } from 'react-router-dom';
